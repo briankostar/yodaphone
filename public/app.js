fe3da02cb17f2a9d263e059abde84c6ec5418f6f
briankostar <brian.kostar@gmail.com>
2014-07-31 04:12:22 -0400

    Basic API function

--- public/app.js
+++ public/app.js
@@ -7,17 +7,9 @@ angular.module('MyApp', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute', 'mg
         templateUrl: 'views/home.html',
         controller: 'MainCtrl'
       })
-      .when('/shows/:id', {
-        templateUrl: 'views/detail.html',
-        controller: 'DetailCtrl'
-      })
-      .when('/login', {
-        templateUrl: 'views/login.html',
-        controller: 'LoginCtrl'
-      })
-      .when('/signup', {
-        templateUrl: 'views/signup.html',
-        controller: 'SignupCtrl'
+      .when('/history', {
+        templateUrl: 'views/history.html',
+        controller: 'HistoryCtrl'
       })
       .when('/add', {
         templateUrl: 'views/add.html',
